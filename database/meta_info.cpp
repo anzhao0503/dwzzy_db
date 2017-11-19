ac69daf5d315f5ffa087ad995933b0aafa06164d
xinchen0104 <jianghechenyan@gmail.com>
2017-11-19 20:43:03 +0800

    Create meta_info.cpp

---
+++ database/meta_info.cpp
@@ -0,0 +1,35 @@
+/*
+ * meta_info.cpp
+ *
+ *  Created on: Nov 15, 2017
+ *      Author: anzhao
+ */
+# include "meta_info.h"
+
+MetaInfo::MetaInfo() {
+	db_meta = new DBMeta();
+}
+
+MetaInfo::~MetaInfo() {
+	delete db_meta;
+}
+
+int TypeMalloc(void*& p, AttrType t, int len = 0){
+	switch(t){
+	case INTEGER:
+		p = (int64_t*)malloc(sizeof(int64_t));
+		break;
+	case FLOAT:
+		p = (float*)malloc(sizeof(float));
+		break;
+	case DATE:
+		p = (int64_t*)malloc(sizeof(int64_t));
+		break;
+	case CHAR:
+		p = (char*)malloc(sizeof(char)*len);
+		break;
+	case VARCHAR:
+		p = (char*)malloc(sizeof(char)*len);
+	}
+	return 0;
+}
